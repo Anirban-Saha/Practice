5f5860d5ff68a15c0cc320c454e5078e0c445678
Anirban Saha <anirbansaha0101@gmail.com>
2022-09-12 01:13:15 +0530

    .

--- C/Graphs/1.c
+++ C/Graphs/1.c
@@ -4,26 +4,51 @@
 int adj[max][max];      // Stores the adjacency matrix
 int q[max] = {0};       // Stores the elements in the queue
 int visited[max] = {0}; // Stores the elements after they are visited
-int n, front = 0, rear = -1; 
+int n, front = 0, rear = -1;
 /*
 n--------> The number of nodes
 n--------> The number of nodes
 n--------> The number of nodes
 
 */
-void bfs(int v);
+void bfs(int v)
+{
+    for (int i = 0; i < n; i++)
+        if (adj[v][i] == 1 && visited[i] == 0)
+            q[++rear] = i;
+    if (front <= rear)
+    {
+        visited[q[front]] = 1;
+        bfs(q[front++]);
+    }
+}
 
-
-int main(void){
-     printf("Enter the number of Vertices\n");
-     scanf("%d",&n);
-     int adj[n][n];
-     printf("\nEnter the graph data in matrix form:\n");
-     for(int i=0; i<n; i++){
-        for(int j=0;j<n;j++){
-            scanf("%d",&adj[i][j]);
+int main(void)
+{
+    int v;
+    printf("Enter the number of Vertices\n");
+    scanf("%d", &n);
+    int adj[n][n];
+    printf("\nEnter the graph data in matrix form:\n");
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d", &adj[i][j]);
         }
-     }
-    
-
+    }
+    printf("Enter the starting node:\n");
+    scanf("%d", &v);
+    bfs(v);
+    printf("The traversed elements are: \n");
+    for (int i = 0; i < n; i++)
+    {
+        if (visited[i] == 1)
+            printf("%d\t", i);
+        else
+        {
+            printf("BFS NOT POSSIBLE!");
+            break;
+        }
+    }
 }
---
+++ C/Graphs/2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#define max 20
+int adj[max][max], visited[max]={0};
+int q[max] = {0}; 
+void dfs(int val){
+    int rear=-1, front = -1, i=0;
+    
+}
