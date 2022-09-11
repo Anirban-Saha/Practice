ea263be027e157e7d0e19fec7ba358a2415e73fb
Anirban Saha <anirbansaha0101@gmail.com>
2022-09-12 01:49:45 +0530

    .

--- C/Graphs/2.c
+++ C/Graphs/2.c
@@ -1,8 +1,39 @@
-#include<stdio.h>
+#include <stdio.h>
 #define max 20
-int adj[max][max], visited[max]={0};
-int q[max] = {0}; 
-void dfs(int val){
-    int rear=-1, front = -1, i=0;
-    
+int adj[max][max], visited[max] = {0};
+int q[max] = {0};
+void bfs(int val)
+{
+    int rear = -1, front = -1, i = 0, queue[max];
+    queue[++rear] = val;
+    visited[val] = 1;
+    while (rear != front)
+    {
+        val = queue[++front];
+        if (val == 4)
+            printf("5\t");
+        else
+            printf("%c\t", val + 65);
+        for (i = 0; i < max; i++)
+        {
+            if (adj[val][i] == i && visited[i] == 0)
+            {
+                queue[++rear] = i;
+                visited[i] = 1;
+            }
+        }
+    }
+}
+int main()
+{
+    printf("\nEnter the graph data in matrix form:\n");
+    for (int i = 0; i < max; i++)
+    {
+        for (int j = 0; j < max; j++)
+        {
+            scanf("%d", &adj[i][j]);
+        }
+    }
+    bfs(0);
+    return 0;
 }
